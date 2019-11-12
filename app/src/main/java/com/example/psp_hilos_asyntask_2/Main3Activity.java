0f532429c542d6e9f878f3d09d8c4a579a5cc278
pedrojesus07 <pedrojesus_95_baza@hotmail.com>
2019-11-12 12:19:08 +0100

    1

--- app/src/main/java/com/example/psp_hilos_asyntask_2/Main3Activity.java
+++ app/src/main/java/com/example/psp_hilos_asyntask_2/Main3Activity.java
@@ -2,13 +2,36 @@ package com.example.psp_hilos_asyntask_2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Main3Activity extends AppCompatActivity {
+
+    private Button bt3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
+
+        init();
+        pulsar();
+    }
+
+    private void pulsar() {
+        bt3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Main3Activity.this, MainActivity.class);
+                startActivity(intent);
+
+            }
+        });
+    }
+
+    private void init() {
+        bt3 = findViewById(R.id.bt3);
     }
 }
